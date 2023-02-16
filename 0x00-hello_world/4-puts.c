3ee276fc1a3dd5730a9302badb68fc955177a625
Hamdallahsj <hfsulaimon@gmail.com>
2023-02-16 14:21:54 -0800

    Update puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,13 +5,13 @@
 */
 int main(void)
 {
-        /* Initializing strings */
-        char value[] = "\"Programming is like building a multilingual puzzle";
-
-        /**
-	 * writing to stdout 
-	 */
-        puts(value);
-
-        return(0);
+ 	/* Initializing strings */
+	char value[] = "\"Programming is like building a multilingual puzzle";
+	
+	/**
+	* writing to stdout 
+	*/
+	puts(value);
+	
+	return (0);
 }
