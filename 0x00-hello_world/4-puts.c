6ce9cda08d2ac9db36734e13ff2971dc607966f2
Hamdallahsj <hfsulaimon@gmail.com>
2023-02-16 13:40:49 -0800

    Update to comply with betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
 int main(){
+	
+	/* Initializing strings */
 	char value[] = "Programming is like building a multilingual puzzle";
+	
+	/* writing to stdout */
 	puts(value);
+
 	return 0;
 }
