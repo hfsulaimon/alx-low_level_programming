827dc7589cd702cfc236483e6704e0bd32b32960
Hamdallahsj <hfsulaimon@gmail.com>
2023-02-17 03:49:11 -0800

    Working with printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - the entry point of C program
+* Return: void returns no value
+*/
+int main(void)
+{
+	/**
+	 * printf() - displays output
+	 */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
