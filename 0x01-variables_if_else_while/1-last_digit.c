2695ea4e4564def2be4b05082968831a54254aeb
Hamdallahsj <hfsulaimon@gmail.com>
2023-02-18 04:28:32 -0800

    if/else with logical operators

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* 
+* main - entry point of C program
+* Return: nothing
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* Get the last digit of n */
+	int last_digit = n % 10;
+	/* if - conditional statement */
+	if (n > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	}
+	else if (n == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
+	else if ((n < 6 )&&(n != 0))
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	}
+	return (0);
+}
