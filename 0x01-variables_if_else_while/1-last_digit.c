0562157938e6fa77d29ab4b97af70cd0101eb45a
Hamdallahsj <hfsulaimon@gmail.com>
2023-02-18 08:56:35 -0800

    if/else with logical operators

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,17 +16,18 @@ int main(void)
 	/* Get the last digit of n */
 	last_digit = n % 10;
 	/* if - conditional statement */
+	printf("Last digit of %d is ", n);
 	if (last_digit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+		printf("%d and is greater than 5\n", last_digit);
 	}
 	else if (last_digit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+		printf("%d and is 0\n", last_digit);
 	}
 	else if ((last_digit < 6) && (last_digit != 0))
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("%d and is less than 6 and not 0\n", last_digit);
 	}
 	return (0);
 }
