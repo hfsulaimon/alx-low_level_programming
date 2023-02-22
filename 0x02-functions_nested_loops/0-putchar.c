2bb5a5a53bff378fdb7c4a591f3a456886c772c4
Hamdallahsj <hfsulaimon@gmail.com>
2023-02-22 01:45:52 -0800

    Update print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
 */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char *words = "_putchar";
+	int i = 0;
+
+	while(i < 9)
+	{
+		_putchar(words[i]);
+		i++;
+	}
 	_putchar('\n');
 	return (0);
 }
