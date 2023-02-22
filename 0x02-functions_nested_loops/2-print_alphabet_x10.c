d552642159a00d99f0484776924c81896ff8a37a
Hamdallahsj <hfsulaimon@gmail.com>
2023-02-22 03:21:20 -0800

    {rints aplhabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,10 @@ void print_alphabet_x10(void)
 	char alpha;
 	int i = 0;
 
-	while(i < 10)
+	while (i < 10)
 	{
 		alpha = 'a';
-		while((int)alpha < 123)
+		while ((int)alpha < 123)
 		{
 			_putchar(alpha);
 			alpha++;
