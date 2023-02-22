4cb06ab659007f0e9659af7825236c9f61b6d42e
Hamdallahsj <hfsulaimon@gmail.com>
2023-02-22 03:16:52 -0800

    {rints aplhabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ * @void - returns nothing
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i = 0;
+
+	while(i < 10)
+	{
+		alpha = 'a';
+		while((int)alpha < 123)
+		{
+			_putchar(alpha);
+			aplha++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
