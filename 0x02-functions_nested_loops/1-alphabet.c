e37192c0abdef5cc1ebfeb5f263774674f28fb08
Hamdallahsj <hfsulaimon@gmail.com>
2023-02-22 02:48:12 -0800

    Update main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print)alphabet - prints alphabets in lower case
+ * @void - return nothing
+ *
+ * Return: nothing
+ */
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while((int)alpha < 123)
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
