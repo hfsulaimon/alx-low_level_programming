095dad10986a770c1be1130d29dec7d7052b24dd
Hamdallahsj <hfsulaimon@gmail.com>
2023-02-23 05:11:07 -0800

    Print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_numbers - Prints from 0-9
+  *
+  * Return: no value
+  */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
+
