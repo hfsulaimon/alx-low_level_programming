323e77fb30d95a28f266d16006ba6b6386598d2e
Hamdallahsj <hfsulaimon@gmail.com>
2023-02-23 05:54:58 -0800

    Prints from 0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * more_numbers - Prints from 0-14
+  *
+  * Return: no value
+  */
+void more_numbers(void)
+{
+	int i= 0, j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 15)
+		{
+			if (j >= 10)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
