4d595abc187ca9ae68cfd0acf99b2dad5904354f
Hamdallahsj <hfsulaimon@gmail.com>
2023-02-23 07:18:43 -0800

    Print diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,5 +8,32 @@
   */
 void print_diagonal(int n)
 {
+	int num = n, i, j;
 
-
+	if (num <= 0)
+	{
+		_putchar('\n');
+	}
+	else if (num > 0)
+	{
+		i = 0;
+		while (i < num)
+		{
+			j = 0;
+			while (j <= i)
+			{
+				if (j != i)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('\\');
+					_putchar('\n');
+				}
+				j++;
+			}
+			i++;
+		}
+	}
+}
