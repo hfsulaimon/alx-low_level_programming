ca89a4c67c2dfc83bfd5a77e7a19f58593213f28
Hamdallahsj <hfsulaimon@gmail.com>
2023-02-27 14:09:23 -0800

    Print elemnets of n array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,17 +10,24 @@
 void print_array(int *a, int n)
 {
 	int *p = a, size = n, i;
-
-	for (i = 0; i < size; i++)
+	
+	if (size <= 0)
 	{
-		printf("%d", p[i]);
-		if (i < (size - 1))
+		printf("\n");
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
 		{
-			printf(", ");
-		}
-		else
-		{
-			printf("\n");
+			printf("%d", p[i]);
+			if (i < (size - 1))
+			{
+				printf(", ");
+			}
+			else
+			{
+				printf("\n");
+			}
 		}
 	}
 }
