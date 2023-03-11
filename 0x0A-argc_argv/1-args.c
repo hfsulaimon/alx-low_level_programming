994fa88a9e017f014cb66c56b37e7faee48f45be
Hamdallahsj <hfsulaimon@gmail.com>
2023-03-11 13:02:04 -0800

    Print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Starting point of C
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	return (0);
+}
