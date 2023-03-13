6e0bb31547c6a12ba0abf18782dc46ecb609bd2d
Hamdallahsj <hfsulaimon@gmail.com>
2023-03-13 00:35:03 -0700

    Prints addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 /**
  * main - Starting point of C
@@ -12,6 +13,8 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
+	unsigned int j;
+
 	if (argc < 2)
 	{
 		printf("0\n");
@@ -20,15 +23,15 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (isdigit(*argv[i]) == 0)
+			for (j = 0; j < strlen(argv[i]); j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			else
-			{
-				sum = sum + atoi(argv[i]);
-			}
+			sum = sum + atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 	}
