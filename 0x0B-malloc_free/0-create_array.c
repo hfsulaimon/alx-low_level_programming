691be34010ec63470e95e4f3d160fc97e419a18d
Hamdallahsj <hfsulaimon@gmail.com>
2023-03-13 06:00:55 -0700

    Orint array of characrter

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_array - Prints array of chars
+ * @size: size of the array
+ * @c: element of the array
+ * Return: a pointer to a dynamic memory allocation
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	s = malloc((size + 1) * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
