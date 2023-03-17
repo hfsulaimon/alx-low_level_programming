79056adf27a309f96ce54e8fcf1d7f42764b7c70
Hamdallahsj <hfsulaimon@gmail.com>
2023-03-17 08:13:44 -0700

    Allocates an array memory dynamically

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,5 +10,9 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	if ((nmemb == 0) || (size == 0))
+		return (NULL);
+	if (malloc(size * nmemb) == NULL)
+		return (NULL);
 	return ((void *)malloc(size * nmemb));
 }
