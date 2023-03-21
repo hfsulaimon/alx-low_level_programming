b7ab1d62926060b7d0d5bbe44b26d2f195a2f288
Hamdallahsj <hfsulaimon@gmail.com>
2023-03-21 04:08:22 -0700

    Defines a dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - Define dog structure with three elements
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
