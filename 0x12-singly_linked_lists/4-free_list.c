7c4db8352e5a0f786c7bd41eb22d83f9ba84b380
Hamdallahsj <hfsulaimon@gmail.com>
2023-04-01 03:57:21 -0700

    Frees memory

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * free_list - Frees a list
+  *
+  * @head: pointer to the beginning of the lists
+  *
+  * Return: nothing
+  */
+ void free_list(list_t *head)
+{
+	list_t *new;
+
+	new = head;
+	while (new != NULL)
+	{
+		free(new->str);
+		free(new);
+		new = new->next;
+	}
+}
