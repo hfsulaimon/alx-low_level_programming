1698f60455bdfa3b6c097e11a4f737779bbf7b20
Hamdallahsj <hfsulaimon@gmail.com>
2023-04-01 04:25:04 -0700

    Executes a function before the execution of main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,10 +4,10 @@
 
 /**
   * print_b4_main - Prints a string before executing the main function
-  *
+  * @void: no argument
   * Returns: nothing
   */
-void print_b4_main()
+void print_b4_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
