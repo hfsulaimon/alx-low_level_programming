eb0e3a89d13d695b5308535033026fa3d9c7308d
Hamdallahsj <hfsulaimon@gmail.com>
2023-04-04 05:29:22 -0700

    Sums up all data in a lists

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+  * sum_listint - Sums the data in a list
+  * @head: pointer to the head of the list
+  * Return: sum of data
+  */
+int sum_listint(listint_t *head)
+{
+	listint_t *temp;
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	temp = head;
+	while (temp->next != NULL)
+	{
+		sum = sum + temp->n;
+		temp = temp->next;
+	}
+	sum = sum + temp->n; 
+	return (sum);
+}
