68067c591f52f9537d45f96e767288f51b0de516
Hamdallahsj <hfsulaimon@gmail.com>
2023-04-04 16:45:03 -0700

    Insert nth nodein a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,8 +23,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 	}
 	/*Check if index is greater than node count*/
-	if (idx > i || idx < 1)
+	if (idx > i)
 		return (NULL);
+	if (idx == 0)
+	{
+		new = malloc(sizeof(*head));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	temp = *head;
 	i = 1;
 	while (i < idx)
