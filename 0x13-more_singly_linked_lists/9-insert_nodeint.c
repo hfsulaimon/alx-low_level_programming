aba60cbdc693205ca015cb551e70efe123e1f9e4
Hamdallahsj <hfsulaimon@gmail.com>
2023-04-04 15:50:52 -0700

    Insert at nth position of a list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 	}
 	/*Check if index is greater than node count*/
-	if (idx > i)
+	if (idx > i || idx < 1)
 		return (NULL);
 	temp = *head;
 	i = 1;
