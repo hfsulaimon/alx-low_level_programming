b8a829bc0c53d7b275a4212b28ff0a2af9ab758f
Hamdallahsj <hfsulaimon@gmail.com>
2023-04-04 04:46:33 -0700

    Prints an nth node in a list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -21,6 +21,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		temp = temp->next;
 	}
 	/*Check if index is greater than node count*/
+	if (index <= 0)
+		return (NULL);
 	if (index > i)
 		return (NULL);
 	temp = head;
