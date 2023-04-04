c74a98bb3b5c6a3159785aac39f9d979261ffd18
Hamdallahsj <hfsulaimon@gmail.com>
2023-04-04 13:01:26 -0700

    Insert a node at nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	temp = *head;
 	i = 0;
-	while (i < (idx - 1))
+	while (i < idx)
 	{
 		temp = temp->next;
 		i++;
